c54cd8abf5ec8b317d16064c372b1476a0c96078
RomanBarashkov <4eepHr7AiDjaWpv>
2022-01-24 21:48:51 +0200

    eth rpc client in progress

---
+++ pkg/rpc/eth/eth_get_storage_at.go
@@ -0,0 +1,27 @@
+package eth
+
+func GetEthStorageAt(client IEthClient, params []string) (*BaseResponseWithStringResult, error) {
+	request := BaseRequest{
+		JsonRpc: "2.0",
+		Method:  "eth_getStorageAt",
+		Params:  params,
+		Id:      "1",
+	}
+
+	baseRequest, err := client.EncodeBaseRequest(request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Send(baseRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	baseResponse, err := client.DecodeBaseResponseWithStringResult(response)
+	if err != nil {
+		return nil, err
+	}
+
+	return baseResponse, nil
+}
