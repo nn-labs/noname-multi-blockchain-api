71f513b9ed14ff4c44b09739010e799df23f800c
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-01-27 14:44:14 +0200

    add mnemonic validator

--- internal/wallet/dto.go
+++ internal/wallet/dto.go
@@ -3,10 +3,21 @@ package wallet
 import (
 	"github.com/go-playground/validator/v10"
 	"nn-blockchain-api/pkg/errors"
+	"strings"
 )
 
 func Validate(dto interface{}) error {
 	validate := validator.New()
+
+	_ = validate.RegisterValidation("mnemonic", func(fl validator.FieldLevel) bool {
+		mnemonic := strings.Split(fl.Field().String(), " ")
+
+		if len(mnemonic) == 12 || len(mnemonic) == 24 {
+			return true
+		}
+
+		return false
+	})
 
 	if err := validate.Struct(dto); err != nil {
 		if _, ok := err.(*validator.InvalidValidationError); ok {
@@ -24,7 +35,7 @@ func Validate(dto interface{}) error {
 
 type CoinNameDTO struct {
 	Name     string `json:"name" validate:"required"`
-	Mnemonic string `json:"mnemonic"`
+	Mnemonic string `json:"mnemonic" validate:"mnemonic"`
 }
 
 type MnemonicDTO struct {
