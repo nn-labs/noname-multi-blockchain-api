e7c625fab4ae36f3119fa3759c27e25fa3ee5582
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-02-01 10:46:20 +0200

    add test for create and decode transaction

--- internal/bitcoin/service_test.go
+++ internal/bitcoin/service_test.go
@@ -96,7 +96,6 @@ func TestService_StatusNode(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	btcClient := mock_bitcoin.NewMockIBtcClient(controller)
 	btcTxSvc := mock_bitcoin.NewMockTransactionService(controller)
 	btcWalletSvc := mock_bitcoin.NewMockWalletService(controller)
 	btcHealthSvc := mock_bitcoin.NewMockHealthService(controller)
@@ -226,18 +225,16 @@ func TestService_StatusNode(t *testing.T) {
 	dto := &StatusNodeDTO{Network: network}
 
 	tests := []struct {
-		name      string
-		ctx       context.Context
-		btcClient bitcoin.IBtcClient
-		dto       *StatusNodeDTO
-		setup     func(dto *StatusNodeDTO)
-		expect    func(t *testing.T, status *StatusNodeInfoDTO, err error)
+		name   string
+		ctx    context.Context
+		dto    *StatusNodeDTO
+		setup  func(dto *StatusNodeDTO)
+		expect func(t *testing.T, status *StatusNodeInfoDTO, err error)
 	}{
 		{
-			name:      "should return ok",
-			ctx:       context.Background(),
-			btcClient: btcClient,
-			dto:       dto,
+			name: "should return ok",
+			ctx:  context.Background(),
+			dto:  dto,
 			setup: func(dto *StatusNodeDTO) {
 				btcHealthSvc.EXPECT().Status(dto.Network).Return(&status, nil)
 			},
@@ -247,10 +244,9 @@ func TestService_StatusNode(t *testing.T) {
 			},
 		},
 		{
-			name:      "should return failed check node status",
-			ctx:       context.Background(),
-			btcClient: btcClient,
-			dto:       dto,
+			name: "should return failed check node status",
+			ctx:  context.Background(),
+			dto:  dto,
 			setup: func(dto *StatusNodeDTO) {
 				btcHealthSvc.EXPECT().Status(dto.Network).Return(nil, errors.NewInternal("failed check node status"))
 			},
@@ -265,6 +261,198 @@ func TestService_StatusNode(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			tc.setup(tc.dto)
 			w, err := service.StatusNode(tc.ctx, tc.dto)
+			tc.expect(t, w, err)
+		})
+	}
+}
+
+func TestService_CreateTransaction(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	btcTxSvc := mock_bitcoin.NewMockTransactionService(controller)
+	btcWalletSvc := mock_bitcoin.NewMockWalletService(controller)
+	btcHealthSvc := mock_bitcoin.NewMockHealthService(controller)
+
+	service, _ := NewService(&logrus.Logger{}, btcTxSvc, btcWalletSvc, btcHealthSvc)
+
+	tx := "transaction"
+	fee := 0.0000259
+
+	dto := &CreateRawTransactionDTO{
+		Utxo: []struct {
+			TxId     string `json:"txid" validate:"required"`
+			Vout     int64  `json:"vout" validate:"required"`
+			Amount   int64  `json:"amount" validate:"required"`
+			PKScript string `json:"pk_script" validate:"required"`
+		}{
+			{
+				TxId:     "989d301c546841d0ac5c8354c7d78079e3603b089682d1639b2ee1c1a8010c6a",
+				Vout:     1,
+				Amount:   1045428,
+				PKScript: "76a914690cd6356789d30b99063632e0651a8d0c206c7f88ac",
+			},
+		},
+		FromAddress: "mq6Qd7JJKsgBYkMFsGCk24MHMxUkuyTnkU",
+		ToAddress:   "mmfbzo2533SFa34ErmYNY4RdVtfw5XYK1u",
+		Amount:      10000,
+		Network:     "test",
+	}
+
+	tests := []struct {
+		name   string
+		ctx    context.Context
+		dto    *CreateRawTransactionDTO
+		setup  func(dto *CreateRawTransactionDTO)
+		expect func(t *testing.T, createdTx *CreatedRawTransactionDTO, err error)
+	}{
+		{
+			name: "should return ok",
+			ctx:  context.Background(),
+			dto:  dto,
+			setup: func(dto *CreateRawTransactionDTO) {
+				btcTxSvc.EXPECT().CreateTransaction(bitcoin.UTXO(dto.Utxo), dto.FromAddress, dto.ToAddress, dto.Amount, dto.Network).Return(&tx, &fee, nil)
+			},
+			expect: func(t *testing.T, createdTx *CreatedRawTransactionDTO, err error) {
+				assert.Nil(t, err)
+				assert.Equal(t, createdTx.Tx, tx)
+				assert.Equal(t, createdTx.Fee, fee)
+			},
+		},
+		{
+			name: "should return error",
+			ctx:  context.Background(),
+			dto:  dto,
+			setup: func(dto *CreateRawTransactionDTO) {
+				btcTxSvc.EXPECT().CreateTransaction(bitcoin.UTXO(dto.Utxo), dto.FromAddress, dto.ToAddress, dto.Amount, dto.Network).Return(nil, nil, errors.NewInternal("failed to create transaction"))
+			},
+			expect: func(t *testing.T, createdTx *CreatedRawTransactionDTO, err error) {
+				assert.Nil(t, createdTx)
+				assert.Equal(t, err, errors.WithMessage(err, "code: 500; status: internal_error; message: failed to create transaction"))
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup(tc.dto)
+			w, err := service.CreateTransaction(tc.ctx, tc.dto)
+			tc.expect(t, w, err)
+		})
+	}
+}
+
+func TestService_DecodeTransaction(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	btcTxSvc := mock_bitcoin.NewMockTransactionService(controller)
+	btcWalletSvc := mock_bitcoin.NewMockWalletService(controller)
+	btcHealthSvc := mock_bitcoin.NewMockHealthService(controller)
+
+	service, _ := NewService(&logrus.Logger{}, btcTxSvc, btcWalletSvc, btcHealthSvc)
+
+	dto := &DecodeRawTransactionDTO{
+		Tx:      "transaction",
+		Network: "test",
+	}
+
+	decodeTx := &bitcoin.DecodedTx{
+		Txid:     "tx",
+		Hash:     "hash",
+		Version:  0,
+		Size:     0,
+		Vsize:    0,
+		Weight:   0,
+		Locktime: 0,
+		Vin: []struct {
+			Txid      string `json:"txid"`
+			Vout      int    `json:"vout"`
+			ScriptSig struct {
+				Asm string `json:"asm"`
+				Hex string `json:"hex"`
+			} `json:"scriptSig"`
+			Sequence int64 `json:"sequence"`
+		}{
+			{
+				Txid: "tx",
+				Vout: 0,
+				ScriptSig: struct {
+					Asm string `json:"asm"`
+					Hex string `json:"hex"`
+				}{
+					Asm: "asm",
+					Hex: "hex",
+				},
+				Sequence: 0,
+			},
+		},
+		Vout: []struct {
+			Value        float64 `json:"value"`
+			N            int     `json:"n"`
+			ScriptPubKey struct {
+				Asm     string `json:"asm"`
+				Hex     string `json:"hex"`
+				Address string `json:"address"`
+				Type    string `json:"type"`
+			} `json:"scriptPubKey"`
+		}{
+			{
+				Value: 0,
+				N:     0,
+				ScriptPubKey: struct {
+					Asm     string `json:"asm"`
+					Hex     string `json:"hex"`
+					Address string `json:"address"`
+					Type    string `json:"type"`
+				}{
+					Asm:     "asm",
+					Hex:     "hex",
+					Address: "address",
+					Type:    "type",
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		name   string
+		ctx    context.Context
+		dto    *DecodeRawTransactionDTO
+		setup  func(dto *DecodeRawTransactionDTO)
+		expect func(t *testing.T, createdTx *DecodedRawTransactionDTO, err error)
+	}{
+		{
+			name: "should return ok",
+			ctx:  context.Background(),
+			dto:  dto,
+			setup: func(dto *DecodeRawTransactionDTO) {
+				btcTxSvc.EXPECT().DecodeTransaction(dto.Tx, dto.Network).Return(decodeTx, nil)
+			},
+			expect: func(t *testing.T, decodeTx *DecodedRawTransactionDTO, err error) {
+				assert.Nil(t, err)
+				assert.Equal(t, decodeTx.Txid, "tx")
+			},
+		},
+		{
+			name: "should return error",
+			ctx:  context.Background(),
+			dto:  dto,
+			setup: func(dto *DecodeRawTransactionDTO) {
+				btcTxSvc.EXPECT().DecodeTransaction(dto.Tx, dto.Network).Return(nil, errors.NewInternal("failed to decode transaction"))
+			},
+			expect: func(t *testing.T, decodeTx *DecodedRawTransactionDTO, err error) {
+				assert.Nil(t, decodeTx)
+				assert.Equal(t, err, errors.WithMessage(err, "code: 500; status: internal_error; message: failed to decode transaction"))
+
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup(tc.dto)
+			w, err := service.DecodeTransaction(tc.ctx, tc.dto)
 			tc.expect(t, w, err)
 		})
 	}
