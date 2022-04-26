d6605cf3955f260d0d410781caa2f40d2eb41464
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-04-26 13:49:23 +0300

    uodate env example update gitflows

--- env.sh
+++ env.sh
@@ -1,9 +1,11 @@
+cat > app.env << EOF
 PORT=:5000
 APP_ENV=development
-
 GRPC_HOST=localhost
-
 BTC_RPC_ENDPOINT_TEST=localhost
 BTC_RPC_ENDPOINT_MAIN=localhost
 BTC_RPC_USER=user
 BTC_RPC_PASSWORD=password
+ETH_RPC_ENDPOINT_TEST=localhost
+ETH_RPC_ENDPOINT_MAIN=localhost
+EOF
