38fcde5c1f8ddbba01666c537a3b3ca13235e1d4
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-04-28 15:54:17 +0300

    mf

--- pkg/rpc/eth/types.go
+++ pkg/rpc/eth/types.go
@@ -1,5 +1,19 @@
 package eth
 
+type BaseRequest struct {
+	JsonRpc string   `json:"jsonrpc"`
+	Method  string   `json:"method"`
+	Params  []string `json:"params"`
+	Id      string   `json:"id"`
+}
+
+type BaseRequestWithMapParams struct {
+	JsonRpc string            `json:"jsonrpc"`
+	Method  string            `json:"method"`
+	Params  map[string]string `json:"params"`
+	Id      string            `json:"id"`
+}
+
 type BaseResponse struct {
 	Id      string `json:"id"`
 	JsonRpc string `json:"jsonrpc"`
@@ -29,21 +43,4 @@ type StatusNodeResponse struct {
 	StartingBlock string `json:"startingBlock"`
 	CurrentBlock  string `json:"currentBlock"`
 	HighestBlock  string `json:"highestBlock"`
-}
-
-type TransactionByHashResponse struct {
-	BlockHash        string `json:"blockHash"`
-	BlockNumber      string `json:"blockNumber"`
-	From             string `json:"from"`
-	Gas              string `json:"gas"`
-	GasPrice         string `json:"gasPrice"`
-	Hash             string `json:"hash"`
-	Input            string `json:"input"`
-	Nonce            string `json:"nonce"`
-	To               string `json:"to"`
-	TransactionIndex string `json:"transactionIndex"`
-	Value            string `json:"value"`
-	V                string `json:"v"`
-	R                string `json:"r"`
-	S                string `json:"s"`
 }
