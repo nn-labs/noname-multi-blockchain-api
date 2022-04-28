f4b67e66e3f76ae4e9f5ae3aa794a007c2d976cc
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-04-28 15:55:36 +0300

    mf

--- pkg/rpc/eth/types.go
+++ pkg/rpc/eth/types.go
@@ -44,3 +44,20 @@ type StatusNodeResponse struct {
 	CurrentBlock  string `json:"currentBlock"`
 	HighestBlock  string `json:"highestBlock"`
 }
+
+type TransactionByHashResponse struct {
+	BlockHash        string `json:"blockHash"`
+	BlockNumber      string `json:"blockNumber"`
+	From             string `json:"from"`
+	Gas              string `json:"gas"`
+	GasPrice         string `json:"gasPrice"`
+	Hash             string `json:"hash"`
+	Input            string `json:"input"`
+	Nonce            string `json:"nonce"`
+	To               string `json:"to"`
+	TransactionIndex string `json:"transactionIndex"`
+	Value            string `json:"value"`
+	V                string `json:"v"`
+	R                string `json:"r"`
+	S                string `json:"s"`
+}
